a0ea6f99ced8724dede693d5567484435f5fe062
Eric Busalacchi <ebusalacchi@quickbase.com>
2018-06-26 08:01:16 -0700

    update agent to 6.3.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ if (!$packageParams['APIKEY']) { throw "Need to supply an API key!"}
 
 # download location
 $url        = '' # download url, HTTPS preferred
-$url64      = 'https://s3.amazonaws.com/ddagent-windows-stable/ddagent-cli-6.0.3.msi'
+$url64      = 'https://s3.amazonaws.com/ddagent-windows-stable/ddagent-cli-6.3.0.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -19,8 +19,8 @@ $packageArgs = @{
   url64bit      = $url64
   softwareName  = 'Datadog Agent' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
   checksum       = ''
-  checksumType   = 'md5'
-  checksum64     = '5c6bf7fcbf2eef3c86a4a34a4c2b7815'
+  checksumType   = 'sha256'
+  checksum64     = 'ef95edec395120aff2cce34a7eb309759f5da70d5233bdaef0f402c8da419a76'
   checksumType64 = $checksumType
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" APIKEY=`"$($packageParams['APIKEY'])`" HOSTNAME=`"$($packageParams['HOSTNAME'])`" TAGS=`"$($packageParams['TAGS'])`" " # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
